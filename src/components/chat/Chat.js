f6fd6efcb599bb6ab5bc9a8d136cbca39c4ba6f2
Ahmed Halim <ahmedhalim96@bitbucket.org>
2020-05-25 23:36:19 +0200

    Styling main area except messages

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -10,10 +10,29 @@ class Chat extends Component {
 			message: "Hello, From The other side!",
 			date: "11.04.3",
 		},
+		selectedChat: {
+			username: "Ahmed Halim",
+			subTitle: "Yesterday",
+			messages: [
+				{
+					sentby: "Ahmed Halim",
+					text:
+						"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam sint saepe quae quo?",
+				},
+				{
+					sentby: "me",
+					text:
+						"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam sint saepe quae quo?",
+				},
+			],
+		},
 	};
 	render() {
 		let { name, avatar, message, date } = this.state.recentChat;
+		let { username, subTitle, messages } = this.state.selectedChat;
 		let chats = [];
+		let chatMessages = [];
+
 		for (let i = 0; i < 20; i++) {
 			chats.push(
 				<SidebarChat
@@ -37,9 +56,33 @@ class Chat extends Component {
 					<div className="chat__recentchats u-scroller">{chats}</div>
 				</div>
 				<div className="chat__main">
-					<div className="chat__title"></div>
-					<div className="chat__messages"></div>
-					<div className="chat__bar"></div>
+					<div className="chat__title">
+						<div className="chat__title_left">
+							<h2 className="chat__title_main">{username}</h2>
+							<span className="chat__title_subtitle">{subTitle}</span>
+						</div>
+						<div className="chat__title_right">
+							<span className="btn btn-outline">
+								<i className="fa fa-search"></i>
+							</span>
+						</div>
+					</div>
+					<div className="chat__messages">
+						{/* @TODO:create message component */}
+					</div>
+					<div className="chat__bar">
+						<span className="btn btn-outline chat__bar_attachment">
+							<i className="fa fa-paperclip"></i>
+						</span>
+						<input
+							type="text"
+							className="chat__bar_input"
+							placeholder="write a message......"
+						/>
+						<span className=" btn btn-outline chat__bar_emoji">
+							<i className="fa fa-reddit-alien"></i>
+						</span>
+					</div>
 				</div>
 			</div>
 		);
